6c581d668ba8864b15779c08a6dbdede098aa59c
anuwadYO276 <158416918+anuwadYO276@users.noreply.github.com>
2024-06-24 11:09:33 +0700

    up

---
+++ src/app/components/Card1.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+
+const Card1 = (props) => {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card1;
